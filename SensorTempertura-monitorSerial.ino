1b8dff1624abbb6daf6a24da588939f019eaf0a9
adan De santiago <adan1995a@gmail.com>
2016-10-06 17:04:33 -0500

    codigo completo en processing

---
+++ SensorTempertura-monitorSerial.ino
@@ -0,0 +1,42 @@
+
+bool bandera=true;
+float centigrados(){
+float dato;
+float cent;
+dato= analogRead(A1);
+cent=(5.0*dato*100)/1024.0;
+return cent;
+}
+float kelvin(float centigrados){
+  float kelvin;
+  kelvin=centigrados +273.15;
+  return kelvin;
+  }
+
+ float fahrenheit(float centigrados){
+  float fahr;
+  fahr= centigrados*1.8 + 32;
+  return fahr;
+  } 
+void setup() {
+Serial.begin(9600);
+
+
+}
+
+void loop() {
+ float centigra=centigrados();
+ float Kelvin=kelvin(centigra);
+ float Fahr=fahrenheit(centigra);
+ delay(1000);
+ if(bandera){
+ Serial.println("Temperatura");
+ Serial.println("Centigrados\t Kelvin\t Fahrenheit");
+  delay(200);
+  bandera=false;
+ }
+ else{
+  delay(2000);
+  Serial.println(String (centigra)+"\t\t"+ String(Kelvin)+ "\t\t"+String( Fahr));
+  }
+}
